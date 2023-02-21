2db5adfc471225bc74384759858bb00d9a1e5e17
ruswan <sukabumikode@gmail.com>
2023-02-21 11:07:14 +0700

    Penambahan login google

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get all of the socialAccounts for the User
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function socialAccounts()
+    {
+        return $this->hasMany(SocialAccount::class);
+    }
 }
