e2a8a8fcaf3b89c5945fc6d600281bd397589f01
ruswan <sukabumikode@gmail.com>
2023-05-03 15:42:32 +0700

    Buat CRUD User dan Campaign

---
+++ app/Filament/Resources/CampaignResource.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CampaignResource\Pages;
+use App\Filament\Resources\CampaignResource\RelationManagers;
+use App\Models\Campaign;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CampaignResource extends Resource
+{
+    protected static ?string $model = Campaign::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Textarea::make('description')
+                    ->required()
+                    ->maxLength(65535),
+                Forms\Components\TextInput::make('slug')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('image')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('amount')
+                    ->required(),
+                Forms\Components\DateTimePicker::make('expired_at'),
+                Forms\Components\TextInput::make('status')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('description'),
+                Tables\Columns\TextColumn::make('slug'),
+                Tables\Columns\TextColumn::make('image'),
+                Tables\Columns\TextColumn::make('amount'),
+                Tables\Columns\TextColumn::make('expired_at')
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('status'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCampaigns::route('/'),
+            'create' => Pages\CreateCampaign::route('/create'),
+            'edit' => Pages\EditCampaign::route('/{record}/edit'),
+        ];
+    }    
+}
