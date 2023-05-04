e2fe11ef878616d25cf08533b4afeea3f52aba72
ruswan <sukabumikode@gmail.com>
2023-05-04 14:29:07 +0700

    Penambahan campaign status

---
+++ app/Http/Controllers/CampaignStatusController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CampaignStatus;
+use Illuminate\Http\Request;
+
+class CampaignStatusController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\CampaignStatus  $campaignStatus
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CampaignStatus $campaignStatus)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\CampaignStatus  $campaignStatus
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(CampaignStatus $campaignStatus)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\CampaignStatus  $campaignStatus
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CampaignStatus $campaignStatus)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\CampaignStatus  $campaignStatus
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CampaignStatus $campaignStatus)
+    {
+        //
+    }
+}
